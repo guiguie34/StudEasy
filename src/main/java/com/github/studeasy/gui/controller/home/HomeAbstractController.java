dce8b3bc4bd7351c23b1ba1e459c528e7add8b8f
leo <lele.mollard@gmail.com>
2020-12-17 10:24:24 +0100

    Home controllers

---
+++ src/main/java/com/github/studeasy/gui/controller/home/HomeAbstractController.java
@@ -0,0 +1,10 @@
+package com.github.studeasy.gui.controller.home;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeAbstractController  {
+
+
+
+}
---
+++ src/main/java/com/github/studeasy/gui/controller/home/HomePartnerController.java
@@ -0,0 +1,40 @@
+package com.github.studeasy.gui.controller.home;
+
+import com.github.studeasy.gui.routers.AbstractRouter;
+import com.github.studeasy.gui.routers.UserRouter;
+import com.github.studeasy.logic.facades.FacadeUser;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * The controller of the partner view
+ */
+public class HomePartnerController implements Initializable {
+    /**
+     * The router used by the controller
+     */
+    private final AbstractRouter ROUTER;
+
+    /**
+     /**
+     * The facade used by the controller
+     */
+    private final FacadeUser FACADE;
+
+    public HomePartnerController(){
+        this.ROUTER = UserRouter.getInstance();
+        this.FACADE = FacadeUser.getInstance();
+    }
+
+    /**
+     * Function from the interface Initializable
+     * Make changes to the controller and its view before
+     * the view appears on the client side
+     * @param location
+     * @param resources
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+}

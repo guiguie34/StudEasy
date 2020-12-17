c635c60d550a49b826d8c1a9c2fc3f900ccc4e54
xiangjingjing1 <1012985323@qq.com>
2020-12-17 10:59:33 +0100

    Add command of service

---
+++ src/main/java/com/github/studeasy/dao/commandOfServiceDAO/MySQLCommandOfServiceDAO.java
@@ -0,0 +1,8 @@
+package com.github.studeasy.dao.commandOfServiceDAO;
+
+/**
+ * The Command of Service DAO using a MySQL database
+ * Contains all the methods accessing Command of Service related data
+ */
+public class MySQLCommandOfServiceDAO {
+}

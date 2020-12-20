f777f84dae8cecd4d25d74d0d7f0a556642c8e77
leo <lele.mollard@gmail.com>
2020-12-20 21:47:28 +0100

    fix other error about the salt in user

--- src/main/java/com/github/studeasy/logic/common/User.java
+++ src/main/java/com/github/studeasy/logic/common/User.java
@@ -112,7 +112,12 @@ public class User {
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
 
+	public String getSalt() {
+		return salt;
+	}
 
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
 }

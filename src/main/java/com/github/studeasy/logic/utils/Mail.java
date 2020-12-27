98259a16f7a37007e0a9e6bd5bd758b130c24f23
leo <lele.mollard@gmail.com>
2020-12-27 18:28:07 +0100

    throw mail error

--- src/main/java/com/github/studeasy/logic/utils/Mail.java
+++ src/main/java/com/github/studeasy/logic/utils/Mail.java
@@ -27,17 +27,15 @@ public class Mail {
     }
 
     public static void sendMail (String subject, String text, String adrReceiver) throws Exception{
-        try {
-            Message message = new MimeMessage(SESSION);
-            message.setFrom(new InternetAddress(ADDRESS));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(adrReceiver));
-            message.setSubject(subject);
-            message.setText(text);
-            Transport.send(message);
-            System.out.println("Message_envoye");
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
+
+        Message message = new MimeMessage(SESSION);
+        message.setFrom(new InternetAddress(ADDRESS));
+        message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(adrReceiver));
+        message.setSubject(subject);
+        message.setText(text);
+        Transport.send(message);
+        System.out.println("Message_envoye");
+
     }
 }

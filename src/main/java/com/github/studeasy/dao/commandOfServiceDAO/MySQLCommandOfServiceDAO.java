158a34af0d068a232f88921083dd292760e426f2
xiangjingjing1 <1012985323@qq.com>
2020-12-27 17:34:20 +0100

    Merge pointManagement

--- src/main/java/com/github/studeasy/dao/commandOfServiceDAO/MySQLCommandOfServiceDAO.java
+++ src/main/java/com/github/studeasy/dao/commandOfServiceDAO/MySQLCommandOfServiceDAO.java
@@ -1,13 +1,12 @@
 package com.github.studeasy.dao.commandOfServiceDAO;
 
+import com.github.studeasy.dao.exceptions.BadCredentialsException;
 import com.github.studeasy.logic.common.CommandOfService;
 import com.github.studeasy.logic.common.Service;
+import com.github.studeasy.logic.common.User;
 import com.github.studeasy.logic.factory.Factory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * The Command of Service DAO using a MySQL database
@@ -15,9 +14,37 @@ import java.sql.SQLException;
  */
 public class MySQLCommandOfServiceDAO extends CommandOfServiceDAO{
 
+    /**
+     * The connection to the database
+     */
+    private final Connection DB;
+
+    /**
+     * Instantiate the connection db
+     */
+    public MySQLCommandOfServiceDAO() {
+        Factory connection = Factory.getInstance();
+        this.DB = connection.getDb();
+    }
+
     @Override
     public void acceptTransaction(CommandOfService c) throws Exception {
-
+        try {
+            // We prepare the SQL request
+            PreparedStatement preparedStatement;
+            // Will contain the result of the query
+            ResultSet resultSet;
+            String request = "INSERT INTO command(fkUser, fkService, date)"+"VALUES (?,?,?)";
+            preparedStatement = DB.prepareStatement(request);
+            preparedStatement.setInt(1, c.getOwner().getIdUser());
+            preparedStatement.setInt(2, c.getService().getIdService());
+            preparedStatement.setDate(3, (Date) c.getCreationDate());
+            // We execute the query
+            resultSet = preparedStatement.executeQuery();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -37,6 +64,24 @@ public class MySQLCommandOfServiceDAO extends CommandOfServiceDAO{
 
     @Override
     public void addFeedback(CommandOfService c) throws Exception {
-
+        try {
+            // We prepare the SQL request
+            PreparedStatement preparedStatement;
+            // Will contain the result of the query
+            ResultSet resultSet;
+            String request = "INSERT INTO command(fkUser, fkService, titleFeedback, rateFeedback, commentFeedback, date)"+"VALUES (?,?,?,?,?,?)";
+            preparedStatement = DB.prepareStatement(request);
+            preparedStatement.setInt(1, c.getOwner().getIdUser());
+            preparedStatement.setInt(2, c.getService().getIdService());
+            preparedStatement.setString(3,c.getFeedback().getTitle());
+            preparedStatement.setInt(4,c.getFeedback().getRate());
+            preparedStatement.setString(5,c.getFeedback().getComment());
+            preparedStatement.setDate(6, (Date) c.getCreationDate());
+            // We execute the query
+            resultSet = preparedStatement.executeQuery();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }

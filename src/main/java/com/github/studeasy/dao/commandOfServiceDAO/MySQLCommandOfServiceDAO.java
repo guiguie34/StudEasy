315266fd0a688c88d2c9d3d35e7b7c7ec5ea87f0
xiangjingjing1 <1012985323@qq.com>
2020-12-29 00:58:53 +0100

    Update DAO and Facade for CommandOfService

--- src/main/java/com/github/studeasy/dao/commandOfServiceDAO/MySQLCommandOfServiceDAO.java
+++ src/main/java/com/github/studeasy/dao/commandOfServiceDAO/MySQLCommandOfServiceDAO.java
@@ -7,6 +7,9 @@ import com.github.studeasy.logic.common.User;
 import com.github.studeasy.logic.factory.Factory;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * The Command of Service DAO using a MySQL database
@@ -34,11 +37,11 @@ public class MySQLCommandOfServiceDAO extends CommandOfServiceDAO{
             PreparedStatement preparedStatement;
             // Will contain the result of the query
             ResultSet resultSet;
-            String request = "INSERT INTO command(fkUser, fkService, date)"+"VALUES (?,?,?)";
+
+            String request = "UPDATE command SET state = 1 where fkUser = ? and fkService = ?";
             preparedStatement = DB.prepareStatement(request);
             preparedStatement.setInt(1, c.getOwner().getIdUser());
             preparedStatement.setInt(2, c.getService().getIdService());
-            preparedStatement.setDate(3, (Date) c.getCreationDate());
             // We execute the query
             resultSet = preparedStatement.executeQuery();
         }
@@ -49,17 +52,93 @@ public class MySQLCommandOfServiceDAO extends CommandOfServiceDAO{
 
     @Override
     public void declineTransaction(CommandOfService c) throws Exception {
+        try {
+            // We prepare the SQL request
+            PreparedStatement preparedStatement;
+            // Will contain the result of the query
+            ResultSet resultSet;
 
+            String request = "DELETE from command where fkUser = ? and fkService = ?";
+            preparedStatement = DB.prepareStatement(request);
+            preparedStatement.setInt(1, c.getOwner().getIdUser());
+            preparedStatement.setInt(2, c.getService().getIdService());
+            // We execute the query
+            resultSet = preparedStatement.executeQuery();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void applyForService(Service s) throws Exception {
+    public void applyForService(Service s,Object currentUser) throws Exception {
+        // create a java calendar instance
+        Calendar calendar = Calendar.getInstance();
+        // get a java date (java.util.Date) from the Calendar instance.
+        // this java date will represent the current date, or "now".
+        java.util.Date currentDate = calendar.getTime();
+        // create a java.sql.Date from the java.util.Date
+        java.sql.Date date = new java.sql.Date(currentDate.getTime());
+        try {
+            // We prepare the SQL request
+            PreparedStatement preparedStatement;
+            // Will contain the result of the query
+            ResultSet resultSet;
 
+            String request = "INSERT INTO command(fkUser, fkService, date, state)"+"VALUES (?,?,?,?)";
+            preparedStatement = DB.prepareStatement(request);
+            preparedStatement.setInt(1,((User)currentUser).getIdUser() );
+            preparedStatement.setInt(2, s.getIdService());
+            preparedStatement.setDate(3, date);
+            preparedStatement.setInt(4,0);
+            // We execute the query
+            resultSet = preparedStatement.executeQuery();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void buyService(Service s) throws Exception {
+    public void buyService(Service s,Object currentUser) throws Exception {
+        // create a java calendar instance
+        Calendar calendar = Calendar.getInstance();
+        // get a java date (java.util.Date) from the Calendar instance.
+        // this java date will represent the current date, or "now".
+        java.util.Date currentDate = calendar.getTime();
+        // create a java.sql.Date from the java.util.Date
+        java.sql.Date date = new java.sql.Date(currentDate.getTime());
+        try {
+            // We prepare the SQL request
+            PreparedStatement preparedStatement;
+            PreparedStatement preparedStatement1;
+            // Will contain the result of the query
+            ResultSet resultSet;
+            ResultSet resultSet1 = null;
+            String request1 = "SELECT points from user where emailAddress=?";
+            String request = "INSERT INTO command(fkUser, fkService, date, state)"+"VALUES (?,?,?,?)";
+            preparedStatement = DB.prepareStatement(request);
+            preparedStatement1 = DB.prepareStatement(request1);
+            preparedStatement1.setString(1,((User)currentUser).getEmailAddress());
+            preparedStatement.setInt(1,((User)currentUser).getIdUser() );
+            preparedStatement.setInt(2, s.getIdService());
+            preparedStatement.setDate(3, date);
+            preparedStatement.setInt(4,0);
+            // We execute the query
+            resultSet1= preparedStatement1.executeQuery();
+            ResultSetMetaData resultMeta = resultSet1.getMetaData();
+            int pointUser = Integer.parseInt(resultMeta.toString());
+            if(pointUser>s.getCost()){
+                resultSet = preparedStatement.executeQuery();
+            }
+            else{
+                Exception e = new Exception("No enough points");
+            }
 
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/main/java/com/github/studeasy/logic/facades/FacadeCommandOfService.java
+++ src/main/java/com/github/studeasy/logic/facades/FacadeCommandOfService.java
@@ -1,8 +1,51 @@
 package com.github.studeasy.logic.facades;
 
+import com.github.studeasy.dao.commandOfServiceDAO.CommandOfServiceDAO;
+import com.github.studeasy.dao.feedbackDAO.FeedbackDAO;
+import com.github.studeasy.logic.common.CommandOfService;
+
 /**
  * The Facade User for the CommandOfServiceDAO
  * It contains methods that allow to create,update,delete a command
  */
 public class FacadeCommandOfService {
+
+    /**
+     * Singleton of the facade CommandOfServie
+     */
+    private static FacadeCommandOfService facadeCommandOfService= null;
+
+    /**
+     * The DAO connected to the database
+     */
+    private final CommandOfServiceDAO DAO;
+
+    /**
+     * Constructor of singleton FacadeCommandOfService
+     * Instantiate the factory
+     */
+    private FacadeCommandOfService() {
+        // We retrieve the UserDao
+        this.DAO = CommandOfServiceDAO.getInstance();
+    }
+
+    /**
+     * Static function that allow to get the instance of the FacadeCommandOfService
+     * @return the instance of FacadeCommandOfService
+     */
+    public static FacadeCommandOfService getInstance(){
+        if(facadeCommandOfService == null){
+            facadeCommandOfService = new FacadeCommandOfService();
+        }
+        return facadeCommandOfService;
+    }
+
+    /***
+     *
+     * @param c
+     */
+    public void acceptTransaction(CommandOfService c){
+
+    }
+
 }
